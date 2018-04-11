63e56ade26dd5c4d95e6593f9c7fec4f7420f457
jiangdydy@gmail.com <609694547@qq.com>
2018-04-11 16:50:14 +0800

    Add some cpp.

---
+++ resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ImageLoad.rc 使用
+//
+#define IDD_IMAGELOAD_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_Folders                     1000
+#define IDC_Buttonnext                  1001
+#define IDC_EDIT1                       1002
+#define IDC_ShowImg                     1003
+#define IDC_BEFORE                      1004
+#define IDC_SSD                         1005
+#define ID_CANCEL                       1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
